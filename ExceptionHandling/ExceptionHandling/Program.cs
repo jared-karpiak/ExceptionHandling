e2b2d21a8d591284761c2da36d565d839810f261
jared-karpiak <jkarpiak@nait.ca>
2025-02-10 15:34:46 -0700

    Adding solution
    Adding solution and initial comments about exception handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Computer programs, if not coded properly or with the correct safeguards,
+            // may throw what are called "Exceptions"
+            // An exception is a type of computer error that occurs when a program is
+            // trying to do something, but can't.
+            // For example, we get an exception when we try an parse a sentence to an int
+           
+            // int.Parse("HELLO");
+            
+            // the above line will throw an exception because the program cannot convert
+            // "HELLO" to an integer.
+
+            // There are hundreds and hundreds of exceptions built into programming languages
+            // and we can even create our own (but we won't).
+        }
+    }
+}
